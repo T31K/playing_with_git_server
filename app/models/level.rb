e7138de4ca40a3740e1dec2af8cf64fcabaf8216
Pablo <pcabezasgt@gmail.com>
2023-09-14 17:07:50 -0300

    feat: level selection and stages configuration

--- app/models/level.rb
+++ app/models/level.rb
@@ -1,2 +1,18 @@
 class Level < ApplicationRecord
+  has_many :stages
+
+  def configure_for_user
+    id = self.id
+    index = self.index
+    name = self.name
+    stages = Stage.where(:level_id => id).order(:step)
+
+    {
+      id: id,
+      index: index,
+      name: name,
+      stages: stages
+    }
+
+  end
 end
